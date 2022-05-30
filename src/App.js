83a4b9a6dc1e3a184b6e1b2ef168fddcb8eac636
Tasfiq97 <tasfiqulazad200@gmail.com>
2022-05-31 01:27:22 +0600

    another commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header></Header>
       <Foods></Foods>
     </div>
